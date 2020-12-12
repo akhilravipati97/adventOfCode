36562631b7ec528f45e69545fc330adbeb741772
Akhil Ravipati <akhil.ravipati.o@gmail.com>
2020-12-12 23:45:16 +0530

    Day 8 part 2 - brute force

--- src/main/day8.cpp
+++ src/main/day8.cpp
@@ -16,34 +16,62 @@ instr parse_instruction(std::vector<std::string>& lines) {
     instructions.reserve(lines.size());
     std::for_each(lines.begin(), lines.end(), [&instructions](auto line) {
         std::string instruction = line.substr(0, 3);
-        int offset = instruction == "nop" ? 0 : std::stoi(line.substr(4, line.size()-4));
+        int offset = std::stoi(line.substr(4, line.size()-4));
         instructions.emplace_back(std::make_pair(instruction, offset));
     });
     return instructions;
 }
 
-void run_part1(std::vector<std::string>& lines) {
-    instr instructions = parse_instruction(lines);
+bool is_loop(instr& instructions, int& acc) {
     std::unordered_set<int> visited;
-    int acc = 0;
     int i = 0;
     std::string instruction;
     int offset;
-    while(!visited.count(i)) {
+    while(!visited.count(i) && i<instructions.size()) {
         visited.emplace(i);
         std::tie(instruction, offset) = instructions[i];
         if(instruction == "jmp") {
             i = i + offset;
+        } else if (instruction == "acc") {
+            acc += offset;
+            i += 1;
         } else {
             i += 1;
-            acc += offset;
         }
     }
+    return i != instructions.size(); 
+}
+
+
+void run_part1(std::vector<std::string>& lines) {
+    instr instructions = parse_instruction(lines);
+    int acc = 0;
+    is_loop(instructions, acc);
     std::cout<<"Last acc value: " << acc <<std::endl;
 }
 
 void run_part2(std::vector<std::string>& lines) {
-
+    instr instructions = parse_instruction(lines);
+    std::string curr_instr;
+    int offset;
+    for(int i=0; i<instructions.size(); i++) {
+        std::tie(curr_instr, offset) = instructions[i];
+        int acc = 0;
+        bool is_lp = true;
+        if(curr_instr == "nop") {
+            instructions[i].first= "jmp";
+            is_lp = is_loop(instructions, acc);
+        } else if (curr_instr == "jmp") {
+            instructions[i].first = "nop";
+            is_lp = is_loop(instructions, acc);
+        }
+        
+        if(!is_lp) {
+            std::cout<<"Found no loop. Acc value: " << acc << std::endl;
+            break;
+        }
+        instructions[i].first = curr_instr;
+    }
 }
 
 void test() {
